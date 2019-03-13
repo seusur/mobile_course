9d72abb54fa7672ccbecf115059176037339dac0
seuser <enelf87@gmail.com>
2019-03-13 15:42:23 +0200

    add_second_task_create_GetClassNumber_test

--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -25,4 +25,22 @@ public class MainClassTest
             actual, expected
         );
     }
+
+    @Test
+    public void testGetClassNumber()
+    {
+        int lesser_number = 45;
+        int actual = Main.getClassNumber();
+        Assert.assertTrue(
+                this.getGreaterErrorMessage(actual, lesser_number),actual > lesser_number
+        );
+    }
+
+    public String getGreaterErrorMessage(int actual, int lesser_number)
+    {
+        return String.format(
+                "The actual number %d from getClassNumber method is not greater then number %d.",
+                actual, lesser_number
+        );
+    }
 }
